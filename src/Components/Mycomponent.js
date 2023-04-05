c0ce182b968c780df43225c9fedc14c51b73dbc8
phuccoder2000 <phuccoder2000.phucvvctk42@gmail.com>
2023-04-05 16:42:22 +0700

    state form

--- src/Components/Mycomponent.js
+++ src/Components/Mycomponent.js
@@ -9,26 +9,35 @@ class MyComponent extends React.Component {
         age: 25
     }
 
-    handleClickme = (event) => {
-        console.log("clickme")
-    
-        this.setState({
-            name: 'phuc01',
-            age: Math.floor(Math.random() * 100) + 1
+    // handleClickme = (event) => {
+    //     console.log("clickme")
 
-        })
-    }
-    handleOnmouseover = (event) => {
-        // console.log(event.pageX)
-    }
+    //     this.setState({
+    //         name: 'phuc01',
+    //         age: Math.floor(Math.random() * 100) + 1
 
-    // JSX  
+    //     })
+    // }
+
+    handleOnchane = (event) => {
+        this.setState(
+            {
+                name: event.target.value
+            }
+        )
+    }
+    handleOnsubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} and i'm  {this.state.age}
-                <button onMouseOver={this.handleOnmouseover}>onMouseOver</button>
-                <button onClick={this.handleClickme}>Clickme</button>
+                <form onSubmit={(event) => this.handleOnsubmit(event)} >
+                    <input type="text" onChange={(event) => this.handleOnchane(event)}></input>
+                    <button >summit</button>
+                </form>
             </div>
         )
     }
